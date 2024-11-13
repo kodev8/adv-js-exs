0f2634bc597bedf2affc292ffd6dcf046afc4134
Kalev Keil <103848034+kodev8@users.noreply.github.com>
2024-11-13 13:57:33 +0100

    chore: included response status codes

---
+++ src/constants/statusCodes.ts
@@ -0,0 +1,19 @@
+const serverError = 500;
+const notFound = 404;
+const badGateway = 502;
+const success = 200;
+const created = 201;
+const unauthorized = 401;
+const badRequest = 400;
+const userAlreadyExists = 409;
+
+export default {
+    serverError,
+    notFound,
+    badGateway,
+    success,
+    created,
+    unauthorized,
+    badRequest,
+    userAlreadyExists,
+};
